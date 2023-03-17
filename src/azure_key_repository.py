c85f64a7dd377b9ea2aef03aacb242adc8a01685
Esta Nagy <nagyesta@gmail.com>
2023-03-17 22:00:25 +0100

    Fix broken build - Specify API version
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- src/azure_key_repository.py
+++ src/azure_key_repository.py
@@ -16,14 +16,14 @@ class AzureKeyRepository(object):
     def encrypt(self, clear_text: str):
         key: KeyVaultKey = self.key_client.get_key(name=self.key_name)
         text_as_bytes: bytes = bytes(clear_text.encode("utf-8"))
-        client: CryptographyClient = CryptographyClient(key=key.id, credential=self.credential)
+        client: CryptographyClient = CryptographyClient(key=key.id, credential=self.credential, api_version="7.3")
         encrypted: EncryptResult = client.encrypt(algorithm=EncryptionAlgorithm.rsa_oaep_256, plaintext=text_as_bytes)
         client.close()
         return encrypted.ciphertext
 
     def decrypt(self, cipher_text: bytes):
         key: KeyVaultKey = self.key_client.get_key(name=self.key_name)
-        client: CryptographyClient = CryptographyClient(key=key.id, credential=self.credential)
+        client: CryptographyClient = CryptographyClient(key=key.id, credential=self.credential, api_version="7.3")
         decrypted: DecryptResult = client.decrypt(algorithm=EncryptionAlgorithm.rsa_oaep_256, ciphertext=cipher_text)
         client.close()
         plaintext_bytes: bytes = decrypted.plaintext
