92008d09d7da7781c89bd476730307d9c1ce3098
Esta Nagy <nagyesta@gmail.com>
2025-10-30 22:38:00 +0100

    fix: Fix broken build (#309)
    - Rework crypto client usage
    - Use transport parameters consistently
    - Simplify Docker setup
    
    Signed-off-by: Esta Nagy <nagyesta@gmail.com>

--- src/azure_key_repository.py
+++ src/azure_key_repository.py
@@ -1,4 +1,5 @@
 from azure.core.credentials import TokenCredential
+from azure.core.pipeline.transport._requests_basic import RequestsTransport
 from azure.keyvault.keys import KeyClient, KeyVaultKey
 from azure.keyvault.keys.crypto import CryptographyClient, EncryptionAlgorithm, EncryptResult, DecryptResult
 
@@ -9,23 +10,16 @@ class AzureKeyRepository(object):
         self.key_client = key_client
         self.key_name = key_name
 
-    key_name: str
-    key_client: KeyClient
-    credential: TokenCredential
-
-    def encrypt(self, clear_text: str):
-        key: KeyVaultKey = self.key_client.get_key(name=self.key_name)
+    def encrypt(self, clear_text: str, client: CryptographyClient):
         text_as_bytes: bytes = bytes(clear_text.encode("utf-8"))
-        client: CryptographyClient = CryptographyClient(key=key.id, credential=self.credential, api_version="7.3")
         encrypted: EncryptResult = client.encrypt(algorithm=EncryptionAlgorithm.rsa_oaep_256, plaintext=text_as_bytes)
-        client.close()
         return encrypted.ciphertext
 
-    def decrypt(self, cipher_text: bytes):
-        key: KeyVaultKey = self.key_client.get_key(name=self.key_name)
-        client: CryptographyClient = CryptographyClient(key=key.id, credential=self.credential, api_version="7.3")
+    def decrypt(self, cipher_text: bytes, client: CryptographyClient):
         decrypted: DecryptResult = client.decrypt(algorithm=EncryptionAlgorithm.rsa_oaep_256, ciphertext=cipher_text)
-        client.close()
         plaintext_bytes: bytes = decrypted.plaintext
         return plaintext_bytes.decode("utf-8")
+
+    def get_key_id(self):
+        return self.key_client.get_key(name=self.key_name).id
 
